040556f24b2b6a57262672871166c90e2e25aa9e
NatWil05 <williams.natasha09@gmail.com>
2023-03-16 11:03:58 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = [" Guadalupe ", " Ollie ", " Aki "];
+
+function writeCards(names, event) {
+    let messages = []
+    for(let i = 0;  i < names.length; i++) {
+        messages.push("Thank you, " + names [i] + "," + " for the wonderful " +  event  + " gift!")
+    }
+    return messages
+} 
+
+const number = ["10"]
+function countDown(number) {
+    let i = [10]
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
+
+console.log(countDown)
